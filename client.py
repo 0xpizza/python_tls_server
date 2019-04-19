f70d183f84bc681a442f7ff0ec5ac1f1ccda5d4d
0xpizza <36284190+0xpizza@users.noreply.github.com>
2019-04-19 10:22:16 -0700

    Add files via upload

---
+++ client.py
@@ -0,0 +1,22 @@
+
+
+# this will query your fancy new python tls web server
+
+
+import requests
+
+r = requests.get('https://localhost:10023', verify=False)
+
+print(r.content.decode())
+
+# alternaatively you can even use your WEB BROWSER!!!!!
+# you'll have to add a security exception for the localhost though,
+# because it's a self-signed certificate and all.
+
+#import webbrowser
+#webbrowser.open('https://localhost:10023')
+
+# enjoy
+
+input('press any key to continue')
+
---
+++ tls_server.py
@@ -0,0 +1,43 @@
+
+'''
+https://docs.python.org/3/library/ssl.html#server-side-operation
+'''
+
+import ssl, socket
+
+key_pass = b'passphrase'
+context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
+context.load_cert_chain( certfile='certificate.pem'
+                        ,keyfile='key.pem'
+                        ,password=key_pass)
+                        
+bindsocket = socket.socket()
+bindsocket.bind(('0.0.0.0', 10023))
+bindsocket.listen(5)
+
+def deal_with_client(connstream):
+    try:
+        # because our cert is self-signed, this step will freak out with an error:
+        # ssl.SSLError: [SSL: TLSV1_ALERT_UNKNOWN_CA] tlsv1 alert unknown ca (_ssl.c:2460)
+        # so we just say "oh well" and send a response anyway. 
+        # you'll need to play around with it to get it to actually read the data.
+        # sometimes "it just works" is good enough for learning purposes :)
+        connstream.recv(1024)
+    except:
+        pass
+    connstream.send(
+        b"HTTP/1.1 200 OK\r\n"
+        b"Content-Type: text/html\r\n"
+        b"\r\n"
+        b"<html> congratulations you did it </html>"
+    )
+    
+print('serving on https://localhost:10023')
+while True:
+    newsocket, fromaddr = bindsocket.accept()
+    connstream = context.wrap_socket(newsocket, server_side=True)
+    try:
+        deal_with_client(connstream)
+    finally:
+        connstream.shutdown(socket.SHUT_RDWR)
+        connstream.close()
